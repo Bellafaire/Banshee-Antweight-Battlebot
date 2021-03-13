3d7db20a57bc5b6da5043678c85f6943b2b9bb06
Matthew Bellafaire <matthewjbella@gmail.com>
2021-03-12 21:41:48 -0500

    created basic DC motor testing firmware

---
+++ firmware/firmware.ino
@@ -0,0 +1,52 @@
+#define MOTOR_1EN 17
+#define MOTOR_1A 16
+#define MOTOR_2A 4
+
+#define MOTOR_2EN 26
+#define MOTOR_3A 27
+#define MOTOR_4A 14
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(MOTOR_1EN, OUTPUT);
+  pinMode(MOTOR_1A, OUTPUT);
+  pinMode(MOTOR_2A, OUTPUT);
+  pinMode(MOTOR_2EN, OUTPUT);
+  pinMode(MOTOR_3A, OUTPUT);
+  pinMode(MOTOR_4A, OUTPUT);
+
+
+}
+
+void writeMotor(int motor, int dir, int speed) {
+  if (motor) {
+    digitalWrite(MOTOR_2EN, speed);
+    digitalWrite(MOTOR_3A, !dir);
+    digitalWrite(MOTOR_4A, dir);
+  } else {
+    digitalWrite(MOTOR_1EN, speed);
+    digitalWrite(MOTOR_1A, !dir);
+    digitalWrite(MOTOR_2A, dir);
+  }
+}
+
+
+void loop() {
+  for (int a = 0; a <= 1; a++) {
+    for (int b = 0; b <= 1; b++) {
+      writeMotor(0, a, b);
+      writeMotor(1, 0, 0);
+      delay(1000);
+    }
+  }
+
+  for (int a = 0; a <= 1; a++) {
+    for (int b = 0; b <= 1; b++) {
+      writeMotor(0, 0, 0);
+      writeMotor(1, a, b);
+      delay(1000);
+    }
+  }
+
+}
+
