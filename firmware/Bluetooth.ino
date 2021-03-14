614c27155dc4297e674176ee9ff42f33ee6adf5f
Matthew Bellafaire <matthewjbella@gmail.com>
2021-03-14 15:29:34 -0400

    created basic bluetooth interface for the battle bot
    TODO: 
    -ESC interface
    -Analog voltage readings

---
+++ firmware/Bluetooth.ino
@@ -0,0 +1,96 @@
+BLECharacteristic *batteryVoltage;
+
+class cb : public BLEServerCallbacks    {
+    void onConnect(BLEServer* pServer) {
+      connected = true;
+      digitalWrite(2, HIGH);
+      Serial.println("BLE Device Connected");
+    }
+    void onDisconnect(BLEServer* pServer) {
+      connected = false;
+      digitalWrite(2, LOW);
+      Serial.println("BLE Device Disconnected");
+    }
+};
+
+class rightMotorCallback : public BLECharacteristicCallbacks  {
+    void onWrite(BLECharacteristic *pCharacteristic) {
+
+      int speed = String(pCharacteristic->getValue().c_str()).toInt();
+      rightMotorSpeed =  speed;
+      Serial.println("**** set right motor speed to: " + String(speed));
+    }
+};
+
+class leftMotorCallback : public BLECharacteristicCallbacks  {
+    void onWrite(BLECharacteristic *pCharacteristic) {
+      int speed = String(pCharacteristic->getValue().c_str()).toInt();
+      leftMotorSpeed =  speed;
+      Serial.println("**** set right motor speed to: " + String(speed));
+    }
+};
+
+
+class weaponMotorCallback : public BLECharacteristicCallbacks  {
+    void onWrite(BLECharacteristic *pCharacteristic) {
+      int speed = String(pCharacteristic->getValue().c_str()).toInt();
+      weaponSpeed =  speed;
+      Serial.println("**** set right motor speed to: " + String(speed));
+    }
+};
+
+
+void initBLE() {
+  BLEDevice::init("Banshee");
+  BLEServer *pServer = BLEDevice::createServer();
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+
+  //add server callback so we can detect when we're connected.
+  pServer->setCallbacks(new cb());
+
+  //right motor
+  BLECharacteristic *rightMotor = pService->createCharacteristic(
+                                    RIGHT_MOTOR_UUID,
+                                    BLECharacteristic::PROPERTY_WRITE
+                                  );
+  rightMotor->setCallbacks(new rightMotorCallback());
+  rightMotor->setValue("0");
+
+  //left motor
+  BLECharacteristic *leftMotor = pService->createCharacteristic(
+                                   LEFT_MOTOR_UUID,
+                                   BLECharacteristic::PROPERTY_WRITE
+                                 );
+  leftMotor->setCallbacks(new leftMotorCallback());
+  leftMotor->setValue("0");
+
+
+  //weapon
+  BLECharacteristic *weaponSpeed = pService->createCharacteristic(
+                                     WEAPON_SPEED_UUID,
+                                     BLECharacteristic::PROPERTY_WRITE
+                                   );
+  weaponSpeed->setCallbacks(new weaponMotorCallback());
+  weaponSpeed->setValue("0");
+
+  //battery voltage
+  batteryVoltage = pService->createCharacteristic(
+                     BATTERY_VOLTAGE_UUID,
+                     BLECharacteristic::PROPERTY_READ   |
+                     BLECharacteristic::PROPERTY_NOTIFY
+                   );
+
+  pService->start();
+  // BLEAdvertising *pAdvertising = pServer->getAdvertising();  // this still is working for backward compatibility
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(true);
+  pAdvertising->setMinPreferred(0x06);  // functions that help with iPhone connections issue
+  pAdvertising->setMinPreferred(0x12);
+  BLEDevice::startAdvertising();
+}
+
+void updateBatteryVoltage() {
+  batteryVoltage->setValue(String(getBatteryVoltage()).c_str());
+  batteryVoltage->notify();
+}
--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,52 +1,96 @@
-#define MOTOR_1EN 17
+/*TODO: 
+ * -ESC interface
+ * -Analog voltage readings
+ */
+
+
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+
+#define SERVICE_UUID        "b2ee9903-faef-4800-970b-28c4f243893f"
+#define RIGHT_MOTOR_UUID         "b2ee9903-faef-4800-970b-28c4f2438940"
+#define LEFT_MOTOR_UUID          "b2ee9903-faef-4800-970b-28c4f2438941"
+#define WEAPON_SPEED_UUID        "b2ee9903-faef-4800-970b-28c4f2438942"
+#define BATTERY_VOLTAGE_UUID     "b2ee9903-faef-4800-970b-28c4f2438943"
+void initBLE();
+
+boolean connected = false;
+int rightMotorSpeed = 0;
+int leftMotorSpeed = 0;
+int weaponSpeed = 0;
+
+unsigned long lastBatteryVoltageUpdate = 0;
+#define BATTERY_VOLTAGE_UPDATE_RATE 1000 //ms between updates
+
+#define MOTOR1_EN 17
+#define MOTOR1_CHANNEL 0
 #define MOTOR_1A 16
 #define MOTOR_2A 4
 
-#define MOTOR_2EN 26
+#define MOTOR2_EN 26
+#define MOTOR2_CHANNEL 1
 #define MOTOR_3A 27
 #define MOTOR_4A 14
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(MOTOR_1EN, OUTPUT);
+  Serial.begin(115200);
+  initBLE();
+  //PWM Config
+  ledcSetup(MOTOR1_CHANNEL, 1000, 8);
+  ledcSetup(MOTOR2_CHANNEL, 1000, 8);
+  ledcAttachPin(MOTOR1_EN, MOTOR1_CHANNEL);
+  ledcAttachPin(MOTOR2_EN, MOTOR2_CHANNEL);
+
+  //standard IO config
   pinMode(MOTOR_1A, OUTPUT);
   pinMode(MOTOR_2A, OUTPUT);
-  pinMode(MOTOR_2EN, OUTPUT);
   pinMode(MOTOR_3A, OUTPUT);
   pinMode(MOTOR_4A, OUTPUT);
-
-
+  pinMode(2, OUTPUT); //DevkitC blue LED
 }
 
-void writeMotor(int motor, int dir, int speed) {
+void writeMotor(int motor, int speed) {
   if (motor) {
-    digitalWrite(MOTOR_2EN, speed);
-    digitalWrite(MOTOR_3A, !dir);
-    digitalWrite(MOTOR_4A, dir);
+    ledcWrite(MOTOR2_CHANNEL, abs(speed));
+    digitalWrite(MOTOR_3A, speed > 0 );
+    digitalWrite(MOTOR_4A, speed < 0 );
   } else {
-    digitalWrite(MOTOR_1EN, speed);
-    digitalWrite(MOTOR_1A, !dir);
-    digitalWrite(MOTOR_2A, dir);
+    ledcWrite(MOTOR1_CHANNEL, abs(speed));
+    digitalWrite(MOTOR_1A, speed > 0);
+    digitalWrite(MOTOR_2A, speed < 0);
   }
 }
 
+void setWeaponSpeed(int speed) {
+
+}
+
+float getBatteryVoltage() {
+  return 0.0;
+}
+
+unsigned long lastDebugString = 0;
 
 void loop() {
-  for (int a = 0; a <= 1; a++) {
-    for (int b = 0; b <= 1; b++) {
-      writeMotor(0, a, b);
-      writeMotor(1, 0, 0);
-      delay(1000);
+  if (connected) {
+    writeMotor(0, rightMotorSpeed);
+    writeMotor(1, leftMotorSpeed);
+    setWeaponSpeed(weaponSpeed);
+
+    if (BATTERY_VOLTAGE_UPDATE_RATE + lastBatteryVoltageUpdate < millis()) {
+      lastBatteryVoltageUpdate = millis();
+      updateBatteryVoltage();
     }
+  } else {
+    writeMotor(0, 0);
+    writeMotor(1, 0);
+    setWeaponSpeed(0);
   }
 
-  for (int a = 0; a <= 1; a++) {
-    for (int b = 0; b <= 1; b++) {
-      writeMotor(0, 0, 0);
-      writeMotor(1, a, b);
-      delay(1000);
-    }
+  if (500 + lastDebugString < millis()) {
+    lastDebugString = millis();
+    Serial.println("Connected: " + String(connected) + " Left Motor Speed: " + String(leftMotorSpeed) + " Right Motor Speed: " + String(rightMotorSpeed) + + " Weapon Motor Speed: " + String(weaponSpeed));
   }
 
 }
-
